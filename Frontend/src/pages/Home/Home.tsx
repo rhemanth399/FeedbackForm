947d0638e6e947074f5399132e32a31eb038f33d
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2024-08-28 17:13:56 +0530

    "frontend updated"

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import Link1 from '../../components/Link1/Link1';
-import Link2 from '../../components/Link2/Link2';
-import { useLocation,Link } from 'react-router-dom';
+// import React from 'react';
+// import Link1 from '../../components/Link1/Link1';
+// import Link2 from '../../components/Link2/Link2';
+// import { useLocation,Link } from 'react-router-dom';
+
+// const Home: React.FC = () => {
+//     const location = useLocation();
+//     const searchParams = new URLSearchParams(location.search)
+//     const formId = searchParams.get('formId');
+//     return (
+//         <div>
+//             <Link to={`withData?formId=${formId}`}>
+
+//             <Link1/>
+//             </Link>
+            
+//             <Link to={`withoutData?formId=${formId}`}>
+
+//             <Link2/>
+//             </Link>
+//         </div>
+//     );
+// }
+
+// export default Home;
+
+
+
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
     const location = useLocation();
-    const searchParams = new URLSearchParams(location.search)
+    const navigate = useNavigate();
+    const searchParams = new URLSearchParams(location.search);
     const formId = searchParams.get('formId');
-    return (
-        <div>
-            <Link to={`withData?formId=${formId}`}>
 
-            <Link1/>
-            </Link>
-            
-            <Link to={`withoutData?formId=${formId}`}>
+    useEffect(() => {
+        if (formId) {
+            navigate(`/withData?formId=${formId}`);
+        }
+    }, [formId, navigate]);
 
-            <Link2/>
-            </Link>
-        </div>
-    );
+    return null; // Or you can return a loading spinner or a simple message
 }
 
 export default Home;
